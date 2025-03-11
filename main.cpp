90d19fdb4971aaf9a2d067f181011f3e2312726b
mcmarius <23401453+mcmarius@users.noreply.github.com>
2025-03-11 17:39:32 +0200

    Cerințe tema 1

--- main.cpp
+++ main.cpp
@@ -2,9 +2,105 @@
 #include <array>
 
 #include <Helper.h>
+#include <ostream>
+#include <unordered_map>
+#include <vector>
+
+class Student {
+    std::string nume;
+    std::unordered_map<std::string, int> note;
+
+public:
+    Student(const std::string &nume, const std::unordered_map<std::string, int> &note)
+        : nume(nume),
+          note(note) {
+        std::cout << "constr init student " << nume << std::endl;
+    }
+
+    Student(const Student &other)
+        : nume(other.nume),
+          note(other.note) {
+        std::cout << "cc student " << nume << std::endl;
+    }
+
+    Student & operator=(const Student &other) {
+        if (this == &other)
+            return *this;
+        nume = other.nume;
+        note = other.note;
+        std::cout << "op= student " << nume << std::endl;
+        return *this;
+    }
+
+    ~Student() {
+        std::cout << "destr student " << nume << std::endl;
+    }
+
+    [[nodiscard]] const std::string & get_nume() const {
+        return nume;
+    }
+
+    [[nodiscard]] const std::unordered_map<std::string, int> & get_note() const {
+        return note;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Student& student);
+};
+
+class Profesor {
+    std::string nume;
+    std::vector<std::string> materii;
+
+public:
+    Profesor(const std::string &nume, const std::vector<std::string> &materii)
+        : nume(nume),
+          materii(materii) {
+    }
+
+    friend std::ostream & operator<<(std::ostream &os, const Profesor &prof) {
+        os << "nume: " << prof.nume << " materii:\n";
+        for (const auto& materie : prof.materii) {
+            os << materie << "\n";
+        }
+        return os;
+    }
+};
+
+class Facultate {
+    std::string nume;
+    std::vector<Student> studenti;
+    std::vector<Profesor> profi;
+
+public:
+    Facultate() = default;
+
+    explicit Facultate(const std::string &nume)
+        : nume(nume) {
+    }
+
+    Facultate(const std::string &nume, const std::vector<Student> &studenti,
+        const std::vector<Profesor> &profi)
+        : nume(nume),
+          studenti(studenti),
+          profi(profi) {
+    }
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Student& student) {
+    os << student.nume << ":\n";
+    for (const auto& [materie, nota] : student.note) {
+        os << materie << ": " << nota << "\n";
+    }
+    os << std::endl;
+    return os;
+}
 
 int main() {
+    Student st1("abc", {{"oop", 10}}), st2{"def", {}};
+    std::cout << st1 << st2 << std::endl;
+
     std::cout << "Nu mai e mult până la pauză\n";
+    // operator<<(std::cout, "Nu mai e mult până la pauză\n");
     std::array<int, 100> v{};
     int nr;
     std::cout << "Introduceți nr: ";
